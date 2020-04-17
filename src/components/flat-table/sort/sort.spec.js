e1fa73f446252bc9f4b96005fb6ddb5ecad7d16a
dawidzarzycki <dzarzycki@pgs-soft.com>
2020-04-17 10:12:24 +0200

    test(sort): add tests to the component

--- src/components/flat-table/sort/sort.spec.js
+++ src/components/flat-table/sort/sort.spec.js
@@ -1,66 +1,72 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Sort, FlatTableRow } from '..';
-import FlatTable from '../flat-table.component';
-import FlatTableHead from '../flat-table-head/flat-table-head.component';
-import FlatTableBody from '../flat-table-body/flat-table-body.component';
-import FlatTableCell from '../flat-table-cell/flat-table-cell.component';
-import StyledFlatTableSortHeaderStyle from './flat-table-sort-header.style';
+import Sort from './sort.component';
+import Icon from '../../icon';
 
 describe('FlatTableSortHeader', () => {
-  let wrapper;
-  let onClickFn;
-  let onKeyDownFn;
-  const enterKey = { keyCode: 13, which: 13 };
-  const spaceKey = { keyCode: 32, which: 32 };
-  const randomKey = { keyCode: 82, which: 82 };
+  let wrapper, onClickFn, onKeyDownFn;
+
+  const ENTER_KEY = { keyCode: 13, which: 13 };
+  const SPACE_KEY = { keyCode: 32, which: 32 };
+  const RANDOM_KEY = { keyCode: 82, which: 82 };
 
   beforeEach(() => {
     onClickFn = jest.fn();
     onKeyDownFn = jest.fn();
-    wrapper = renderFlatTable({ onClick: onClickFn }, { onKeyDown: onKeyDownFn });
+    wrapper = renderSort({ onClick: onClickFn, onKeyDown: onKeyDownFn });
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should react to space key', () => {
-    wrapper.find(StyledFlatTableSortHeaderStyle).first().simulate('keyDown', spaceKey);
-
-    expect(onClickFn).toHaveBeenCalledTimes(1);
+  it('should render correctly', () => {
+    expect(wrapper.exists()).toBe(true);
   });
 
-  it('should react to enter key', () => {
-    wrapper.find(StyledFlatTableSortHeaderStyle).first().simulate('keyDown', enterKey);
+  it('should not render Icon if sortType does not exist', () => {
+    wrapper = renderSort();
 
-    expect(onClickFn).toHaveBeenCalledTimes(1);
+    expect(wrapper.find(Icon).exists()).toBe(false);
   });
 
-  it('should not react to different key than enter key or space key', () => {
-    wrapper.find(StyledFlatTableSortHeaderStyle).first().simulate('keyDown', randomKey);
+  it('should render Icon `sort_up` sortType="asc"', () => {
+    wrapper = renderSort({ sortType: 'asc' });
 
-    expect(onClickFn).toHaveBeenCalledTimes(0);
+    expect(wrapper.find(Icon).props().type).toBe('sort_up');
+  });
+
+  it('should render Icon `sort_down` sortType="desc"', () => {
+    wrapper = renderSort({ sortType: 'desc' });
+
+    expect(wrapper.find(Icon).props().type).toBe('sort_down');
+  });
+
+  it('should run callback if clicked', () => {
+    wrapper.props().onClick();
+
+    expect(onClickFn).toHaveBeenCalled();
+  });
+
+  it('shouold fired callback if enter key is pressed', () => {
+    wrapper.simulate('keyDown', ENTER_KEY);
+
+    expect(onClickFn).toHaveBeenCalled();
+  });
+
+  it('should fired callback if space key is pressed', () => {
+    wrapper.simulate('keyDown', SPACE_KEY);
+
+    expect(onClickFn).toHaveBeenCalled();
+  });
+
+  it('should not fired callback if either enter key or space key is pressed', () => {
+    wrapper.simulate('keyDown', RANDOM_KEY);
+
+    expect(onKeyDownFn).not.toHaveBeenCalled();
   });
 });
 
-function renderFlatTable(props, styledSortProps, renderer = mount) {
-  return renderer(
-    <FlatTable>
-      <FlatTableHead>
-        <FlatTableRow>
-          <FlatTableSortHeader { ...props }>
-            <StyledFlatTableSortHeaderStyle { ...styledSortProps }>
-              header 1
-            </StyledFlatTableSortHeaderStyle>
-          </FlatTableSortHeader>
-        </FlatTableRow>
-      </FlatTableHead>
-      <FlatTableBody>
-        <FlatTableRow>
-          <FlatTableCell>cell2</FlatTableCell>
-        </FlatTableRow>
-      </FlatTableBody>
-    </FlatTable>
-  );
+function renderSort(props, renderer = mount) {
+  return renderer(<Sort { ...props }>Name</Sort>);
 }
